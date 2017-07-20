e4d44b20a90f1d3a18b5c2819c3ca0d2ae83a49e
Steve Streeting <steve@stevestreeting.com>
2017-07-20 11:22:14 +0100

    Initial commit demonstrating problem

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startup : MonoBehaviour {
+
+    public ParticleSystem particlePrefab;
+
+	void Start () {
+        var p = GameObject.Instantiate<ParticleSystem>(particlePrefab);
+        p.transform.position = new Vector3(-100, 10, 0);
+        p.Play();
+	}
+}
